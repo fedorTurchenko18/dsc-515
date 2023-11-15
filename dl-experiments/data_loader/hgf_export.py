1881047c8692de4634efe190403e1e88c4bbf06a
fedor.turchenko <fedor.turchenko@somin.ai>
2023-11-16 01:58:17 +0200

    massive repo clean up

--- dl-experiments/data_loader/hgf_export.py
+++ dl-experiments/data_loader/hgf_export.py
@@ -1,23 +1,33 @@
-import cv2, numpy as np, pandas as pd, random, tensorflow as tf
-from typing import Tuple, Literal, List
-from huggingface_hub import login, logout
+import cv2, numpy as np, pandas as pd, random, tensorflow as tf, pickle, json
+from typing import Tuple, Literal, List, Optional, Union
+from huggingface_hub import login, logout, hf_hub_download, HfApi, CommitOperationAdd
 from bertopic import BERTopic
+from top2vec import Top2Vec
 from datasets import load_dataset
 from collections import Counter
 
 
 class HGFresource:
 
-    def __init__(self, token):
+    def __init__(self, token: str):
+        '''
+        token: str
+            HuggingFace access token generated at https://huggingface.co/settings/tokens
+        
+        n_classes: Optional[int]
+            Number of classes of target variable
+        '''
         # keep private
         self.__token = token
+
 
     def load_data_tfds(
             self,
             repo: str,
+            n_classes: int,
             batch_size: int,
-            perform_split: bool = False,
-            train_size: float = None
+            perform_split: Optional[bool] = False,
+            train_size: Optional[float] = None
     ) -> Tuple:
         '''
         Load datasets from Hugging Face organization repository as a Tensorflow MapDataset object
@@ -27,6 +37,10 @@ class HGFresource:
 
         repo : str
             Path to the data repo on Hugging Face
+
+        n_classes: Optional[int]
+            Number of classes of target variable \n
+            Different versions of dataset have different one, so should be specified
 
         batch_size : int
             Desired batch size for the model training
@@ -41,7 +55,7 @@ class HGFresource:
 
         Since the object returned by this function is somewhat unfamiliar (both images and labels combined into a single structure),
         below is the quickstart code example (also available in the load_as_tfds.ipynb):
-        
+
         ```
         import os, numpy as np, tensorflow as tf
         from data_loader.hgf_export import HGFresource
@@ -57,9 +71,9 @@ class HGFresource:
         # Default data load
         train_data, test_data = hgf.load_data_tfds(repo=HGF_DATA_REPO, batch_size=32)
 
-        # Data load with additional split of the train data 
+        # Data load with additional split of the train data
         train_data_split, val_data_split, test_data_split = hgf.load_data_tfds(repo=HGF_DATA_REPO, batch_size=32, perform_split=True, train_size=0.7)
-        
+
         OPTIMIZER = 'adam'
         LOSS = 'categorical_crossentropy'
         METRICS = [tf.keras.metrics.F1Score('weighted')]
@@ -101,44 +115,44 @@ class HGFresource:
         test_data = dataset['test']
 
         if perform_split == False:
-            train_data = self.__process_dataset(train_data, batch_size, perform_split, train_size)
-            test_data = self.__process_dataset(test_data, batch_size, perform_split, train_size)
+            train_data = self.__process_dataset(train_data, n_classes, batch_size, perform_split, train_size)
+            test_data = self.__process_dataset(test_data, n_classes, batch_size, perform_split, train_size)
             return train_data, test_data
-        
+
         else:
             train_data, val_data = self.__process_dataset(train_data, batch_size, True, train_size)
             test_data = self.__process_dataset(test_data, batch_size, False, None)
             return train_data, val_data, test_data
-        
 
-    def __process_dataset(self, dataset, batch_size, perform_split, train_size):
+
+    def __process_dataset(self, dataset, n_classes, batch_size, perform_split, train_size):
         def preprocess_images(examples):
             examples['image'] = [tf.cast(image.convert('RGB'), tf.float32) / 255.0 for image in examples['image']]
             return examples
 
         def preprocess_labels(example):
-            zeros = np.zeros(29)
+            zeros = np.zeros(n_classes)
             np.put(zeros, example, 1)
             zeros = tf.convert_to_tensor(zeros)
             example = {'label': zeros}
             return example
-        
+
         if perform_split == False:
             dataset = dataset.map(preprocess_labels, input_columns=['label'])
             dataset = dataset.with_transform(preprocess_images, ['image'], True)
 
             dataset = dataset.to_tf_dataset(
-                columns="image",
-                label_cols="label",
+                columns='image',
+                label_cols='label',
                 batch_size=batch_size,
                 shuffle=True,
                 prefetch=False
             )
             return dataset
-        
+
         else:
-            dataset = dataset.train_test_split(train_size=train_size, stratify_by_column="label", seed=515)
-            
+            dataset = dataset.train_test_split(train_size=train_size, stratify_by_column='label', seed=515)
+
             train = dataset['train']
             test = dataset['test']
 
@@ -146,8 +160,8 @@ class HGFresource:
             train = train.with_transform(preprocess_images, ['image'], True)
 
             train = train.to_tf_dataset(
-                columns="image",
-                label_cols="label",
+                columns='image',
+                label_cols='label',
                 batch_size=batch_size,
                 shuffle=True,
                 prefetch=False
@@ -155,16 +169,16 @@ class HGFresource:
 
             test = test.map(preprocess_labels, input_columns=['label'])
             test = test.with_transform(preprocess_images, ['image'], True)
-            
+
             test = test.to_tf_dataset(
-                columns="image",
-                label_cols="label",
+                columns='image',
+                label_cols='label',
                 batch_size=batch_size,
                 shuffle=True,
                 prefetch=False
             )
             return train, test
-        
+
 
     def load_data(self, repo: str, sets: Literal['train', 'test', 'all'], sample_fraction: float) -> Tuple[np.ndarray]:
         '''
@@ -261,8 +275,8 @@ class HGFresource:
             images = np.concatenate(images, dtype=np.float16)
             labels = labels.loc[INDICES_REMAINED].values
             return images, labels
-        
-    
+
+
     def __sample_from_df(self, labels: pd.Series, sample_fraction: float = 0.5) -> List[int]:
         '''
         Reduce the size of original dataframe through random stratified sampling
@@ -272,7 +286,7 @@ class HGFresource:
 
         labels : pd.Series
             Either train or test labels
-        
+
         sample_fraction : float
             Share of the dataset to return
 
@@ -294,7 +308,7 @@ class HGFresource:
             label: int(DESIRED_SAMPLES * count / N_ROWS)
             for label, count in target_freqs.items()
         }
-        
+
         sampled_data_indices = []
         for label, count in target_freqs.items():
             class_data = labels[labels==label].index.tolist()
@@ -309,7 +323,7 @@ class HGFresource:
 
 
 
-    def load_model(self, repo: str) -> BERTopic:
+    def load_model(self, repo: str, filename: Optional[Union[str, dict]] = None) -> Union[BERTopic, Top2Vec, tf.keras.Sequential]:
         '''
         Load model from Hugging Face organization repository
 
@@ -318,17 +332,175 @@ class HGFresource:
         repo : str
             Path to the model repo on Hugging Face
 
+        filename : Optional[Union[str, dict]]
+            Name of the file inside the model repo on Hugging Face \n
+            It is required for loading files which cannot be exported via
+            `huggingface_hub` methods \n
+            If `dict` (required for model building workflow):
+            ```
+            {
+                'model_weights': 'model_weights_filename.h5',
+                'model_config': 'model_config_filename.json'
+            }
+            ```
+
         Returns
         -------
 
         Model of interest
         '''
-        # for now only BERTopic model is in use
-        # DL models will be saved to Hugging Face later
-        # implementation of downloading these will be executed here
-        if 'topic' in repo:
-            login(token=self.__token)
-            model = BERTopic.load(repo)
-            logout()
+        # custom method is implemented for each type of model loading
+        # due to heterogeneity of their implementations
+        # in terms of integration with huggingface
+        if 'top2vec' not in repo and 'topic' in repo:
+            # BERTopic model
+            model = self.__load_bertopic_model(repo)
+        elif 'top2vec' in repo:
+            # Top2Vec model
+            model = self.__load_top2vec_model(repo, filename)
+        else:
+            # deep learning model
+            model = self.__load_dl_model(repo, filename)
+
+        return model
+    
+
+    def load_file(self, repo: str, filename: str):
+        '''
+        Download any single file from specific repo
+
+        Parameters
+        ----------
+
+        repo : str
+            Path to the file repo on Hugging Face
+
+        filename : str
+            Name of the file of interest inside the repo
+
+        Returns
+        -------
+
+        File of interest
+        '''
+        file = hf_hub_download(repo_id=repo, filename=filename, token=self.__token)
+        return file
+    
+
+    def __load_bertopic_model(self, repo: str) -> BERTopic:
+        '''
+        Load BERTopic model from Hugging Face organization repository
+
+        Parameters
+        ----------
+        repo : str
+            Path to the BERTopic model repo on Hugging Face
+
+        Returns
+        -------
+
+        Trained BERTopic model
+        '''
+        login(token=self.__token)
+        model = BERTopic.load(repo)
+        logout()
+
+        return model
+
+
+    def __load_top2vec_model(self, repo: str, filename: str) -> Top2Vec:
+        '''
+        Load top2vec model from Hugging Face organization repository
+
+        Parameters
+        ----------
+        repo : str
+            Path to the top2vec model repo on Hugging Face
+
+        filename : str
+            Name of the file with serialized top2vec model inside the repo on Hugging Face
+
+        Returns
+        -------
+
+        Trained Top2Vec model
+        '''
+        model_file = self.load_file(repo=repo, filename=filename)
+        with open(model_file, 'rb') as file:
+            model = pickle.load(file)
+
+        return model
+    
+
+    def __load_dl_model(self, repo: str, filename: dict) -> tf.keras.Sequential:
+        '''
+        Load deep learning model from Hugging Face organization repository
+
+        Parameters
+        ----------
+        repo : str
+            Path to the deep learning model repo on Hugging Face
+
+        filename : dict
+            Dictionary of a form:
+            ```
+            {
+                'model_weights': 'model_weights_filename.h5',
+                'model_config': 'model_config_filename.json'
+            }
+            ```
+
+        Returns
+        -------
+
+        Trained deep learning model
+        '''
+        # download weights
+        weights_file = self.load_file(repo=repo, filename=filename['model_weights'])
+        # download config
+        model_config_file = self.load_file(repo=repo, filename=filename['model_config'])
+        # parse config
+        with open(model_config_file, 'r') as json_file:
+            model_config = json_file.read()
+        model_config = json.loads(model_config)
+        # compile model
+        model = tf.keras.Sequential.from_config(model_config)
+        # set weights
+        model.load_weights(weights_file, by_name=True)
         
         return model
+    
+
+    def commit_to_hub(self, repo: str, path_on_local: List[str], path_in_repo: List[str], commit_message: str) -> None:
+        '''
+        Commit changes to repository
+
+        Parameters
+        ----------
+
+        repo : str
+            Path of the repo on Hugging Face which to commit to
+
+        path_on_local : List[str]
+            List of local paths of files which were added/modified locally
+
+        path_in_repo : List[str]
+            List of remote paths of files which should be created/modified in repo
+
+        commit_message : str
+            Message which describes the files creations/modifications
+        '''
+        login(token=self.__token)
+
+        api = HfApi()
+        # no other operation than add/modify is needed
+        operations = [
+            CommitOperationAdd(path_in_repo=repo_path, path_or_fileobj=local_path) for repo_path, local_path in zip(path_in_repo, path_on_local)
+        ]
+        api.create_commit(
+            repo_id=repo,
+            operations=operations,
+            commit_message=commit_message,
+        )
+
+        logout()
