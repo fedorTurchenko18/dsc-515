b605334cb1c7726a2a1a835f7cc00786193555a0
fedor.turchenko <fedor.turchenko@somin.ai>
2023-11-24 00:22:39 +0200

    add test line

--- flower-ec2/flower-dependencies/server/run_server.py
+++ flower-ec2/flower-dependencies/server/run_server.py
@@ -9,6 +9,7 @@ from aws_management.aws_manager import AWSManager
 from dotenv import load_dotenv
 load_dotenv()
 
+print(os.environ)
 if __name__=='__main__':
     cur_abs_path = os.path.abspath('.')
     DIR = f"{cur_abs_path[:cur_abs_path.find('dsc-515')+len('dsc-515')]}/images_houseware"
