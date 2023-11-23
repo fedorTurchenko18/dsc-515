ef98b3e1bdd9e88dbbd461bd2a153a336efd16b8
fedor.turchenko <fedor.turchenko@somin.ai>
2023-11-23 23:38:54 +0200

    debug s3 logging implementation

--- flower-ec2/main.py
+++ flower-ec2/main.py
@@ -1,7 +1,6 @@
 import os, argparse
 from dotenv import load_dotenv
 from aws_management.aws_manager import AWSManager
-from loguru import logger
 
 if __name__=='__main__':
 
@@ -14,8 +13,15 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser(description='Run Flower client with subset of data')
     parser.add_argument('--n_client_instances', type=int, help='Number of Clients', required=True)
+    parser.add_argument('--num_rounds', type=int, help='Number of Federated Learning rounds to run', required=True)
+    parser.add_argument('--strategy', type=str, help='Strategy to initialize Flower Server with. Available: "FedAvg", "FedAvgM", "FedAdaGrad", "FedAdam"', required=True)
+    parser.add_argument('--backend', type=str, help='Backend of Flower Client. Available: "jax", "torch", "tensorflow"', required=True)
+    
     args = parser.parse_args()
     N_CLIENT_INSTANCES = args.n_client_instances
+    NUM_ROUNDS = args.n_client_instances
+    STRATEGY = args.n_client_instances
+    BACKEND = args.n_client_instances
 
     ec2_manager = AWSManager(
         service='ec2',
@@ -28,26 +34,25 @@ if __name__=='__main__':
 
     # add rule to the default security group to open port 8080 and 22
     port_8080_response = ec2_manager.open_port(default_group=True, port_number=8080)
-    logger.info(f'Response of opening port 8080 operation: {port_8080_response}')
     port_22_response = ec2_manager.open_port(default_group=True, port_number=22)
-    logger.info(f'Response of opening port 22 operation: {port_22_response}')
-
-    # create s3 bucket for Flower logs storage
-    # s3_manager = AWSManager(
-    #     service='s3',
-    #     aws_access_key=AWS_ACCESS_KEY,
-    #     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    #     aws_session_token=AWS_SESSION_TOKEN,
-    #     aws_region=AWS_REGION,
-    #     aws_key_pair=AWS_KEY_PAIR
-    # )
-    # s3_response = s3_manager.create_s3_bucket()
 
     # create and run the server ec2 instance
     with open('flower-dependencies/server/server_startup.txt', 'r') as f:
         server_startup_script = f.read()
-    server_startup_script = server_startup_script.replace('DATA_N', str(N_CLIENT_INSTANCES))
-    logger.info('Creating Server instance...')
+
+    for to_replace, replacement in {
+        'NUM_ROUNDS': str(NUM_ROUNDS),
+        'DATA_N': str(N_CLIENT_INSTANCES),
+        'STRATEGY': STRATEGY,
+        'BACKEND': BACKEND,
+        'ACCESS_KEY': AWS_ACCESS_KEY,
+        'SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY,
+        'SESSION_TOKEN': AWS_SESSION_TOKEN,
+        'REGION': AWS_REGION,
+        'KEY_PAIR': AWS_KEY_PAIR
+    }.items():
+        server_startup_script = server_startup_script.replace(to_replace, replacement)
+
     flower_server = ec2_manager.create_instance('server', startup_script=server_startup_script, n_instances=1)
 
     # Extract the public IP address from the response
@@ -58,11 +63,18 @@ if __name__=='__main__':
     with open('flower-dependencies/client/client_startup.txt', 'r') as f:
         client_startup_script = f.read()
 
-    for to_replace, replacement in {'EC2_PUBLIC_IP': server_public_ip_address, 'DATA_N': str(N_CLIENT_INSTANCES)}.items():
+    for to_replace, replacement in {
+        'EC2_PUBLIC_IP': server_public_ip_address,
+        'DATA_N': str(N_CLIENT_INSTANCES),
+        'BACKEND': BACKEND,
+        'ACCESS_KEY': AWS_ACCESS_KEY,
+        'SECRET_ACCESS_KEY': AWS_SECRET_ACCESS_KEY,
+        'SESSION_TOKEN': AWS_SESSION_TOKEN,
+        'REGION': AWS_REGION,
+        'KEY_PAIR': AWS_KEY_PAIR
+    }.items():
         client_startup_script = client_startup_script.replace(to_replace, replacement)
 
-    logger.info(f'Received request to create {N_CLIENT_INSTANCES} Client instances')
     for idx in range(N_CLIENT_INSTANCES):
         client_specific_startup_script = client_startup_script.replace('DATA_INDEX', str(idx))
-        logger.info(f'Creating Client #{idx+1} instance...')
         flower_clients = ec2_manager.create_instance('clients', startup_script=client_specific_startup_script, n_instances=1)
